8cc0ace96407af450a814b018c296341eedc0a87
Ghanashyam Parise <114044967+Shyam-75@users.noreply.github.com>
2024-06-17 21:48:02 +0530

    Add files via upload

---
+++ gui.py
@@ -0,0 +1,75 @@
+import tkinter as tk
+from tkinter import filedialog, Label, Button
+from tensorflow.keras.models import model_from_json
+from PIL import Image, ImageTk
+import numpy as np
+import cv2
+
+def facialExpressionModel(json_file, weights_file):
+    with open(json_file, 'r') as file:
+        loaded_model_json = file.read()
+        model = model_from_json(loaded_model_json)
+    model.load_weights(weights_file)
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+top = tk.Tk()
+top.geometry('800x600')
+top.title('Emotion Detector')
+top.configure(background='#CDCDCD')
+
+label1 = Label(top, background='#CDCDCD', font=('arial', 15, 'bold'))
+sign_image = Label(top)
+
+face_cascade = cv2.CascadeClassifier('D:\emotion_detection\harrcascade_frontalface_default.xml')
+model = facialExpressionModel('model_a1.json', 'model_weights.h5')
+
+Emotion_list = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprised']
+
+def Detect(file_path):
+    image = cv2.imread(file_path)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray_image, 1.3, 5)
+
+    try:
+        for (x, y, w, h) in faces:
+            fc = gray_image[y:y + h, x:x + w]
+            roi = cv2.resize(fc, (48, 48))
+            roi = roi[np.newaxis, :, :, np.newaxis]
+            pred = Emotion_list[np.argmax(model.predict(roi))]
+            print('Predicted Emotion is ' + pred)
+            label1.configure(foreground='#011638', text=pred)
+    except Exception as e:
+        print(e)
+        label1.configure(foreground='#011638', text='Unable to detect')
+
+def show_Detect_button(file_path):
+    detect_b = Button(top, text='Detect Emotion', command=lambda: Detect(file_path), padx=10, pady=5)
+    detect_b.configure(background='#364156', foreground='white', font=('arial', 10, 'bold'))
+    detect_b.place(relx=0.79, rely=0.46)
+
+def upload_image():
+    try:
+        file_path = filedialog.askopenfilename()
+        uploaded = Image.open(file_path)
+        uploaded.thumbnail((top.winfo_width() / 2.3, top.winfo_height() / 2.3))
+        im = ImageTk.PhotoImage(uploaded)
+
+        sign_image.configure(image=im)
+        sign_image.image = im
+        label1.configure(text='')
+        show_Detect_button(file_path)
+    except Exception as e:
+        print(e)
+
+upload = Button(top, text='Upload Image', command=upload_image, padx=10, pady=5)
+upload.configure(background='#364156', foreground='white', font=('arial', 20, 'bold'))
+upload.pack(side='bottom', pady=50)
+sign_image.pack(side='bottom', expand=True)
+label1.pack(side='bottom', expand=True)
+
+heading = Label(top, text='Emotion Detection', pady=20, font=('arial', 25, 'bold'))
+heading.configure(background='#CDCDCD', foreground='#364156')
+heading.pack()
+
+top.mainloop()
